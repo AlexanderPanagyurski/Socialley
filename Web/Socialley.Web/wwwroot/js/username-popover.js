f00ec27642661cd9db760400643aedb3f50f0aaa
AlexanderPanagyurski <alexander.panagyurski@gmail.com>
2021-05-03 18:18:50 +0300

    Add mouseenter event on popover

--- Web/Socialley.Web/wwwroot/js/username-popover.js
+++ Web/Socialley.Web/wwwroot/js/username-popover.js
@@ -1,3 +1,15 @@
-﻿    $(document).ready(function () {
-        $('.paragraph-popover').popover();
-    });
+﻿    //$(document).ready(function () {
+    //    $('.paragraph-popover').popover();
+    //});
+$('.paragraph-popover').popover({
+    html: true,
+    trigger: 'hover',
+}).on('hide.bs.popover', function () {
+    if ($(".popover:hover").length) {
+        return false;
+    }
+});
+
+$('body').on('mouseleave', '.popover', function () {
+    $('.popover').popover('hide');
+});
