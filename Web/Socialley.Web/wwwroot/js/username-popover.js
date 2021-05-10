8b1469344e8574f7668654a106c5cdc718354154
AlexanderPanagyurski <alexander.panagyurski@gmail.com>
2021-05-10 20:19:00 +0300

    Remove jQuery conflicts

--- Web/Socialley.Web/wwwroot/js/username-popover.js
+++ Web/Socialley.Web/wwwroot/js/username-popover.js
@@ -1,15 +1,16 @@
 ﻿    //$(document).ready(function () {
     //    $('.paragraph-popover').popover();
-    //});
-$('.paragraph-popover').popover({
+//});
+var $y = jQuery.noConflict();
+$y('.paragraph-popover').popover({
     html: true,
     trigger: 'hover',
 }).on('hide.bs.popover', function () {
-    if ($(".popover:hover").length) {
+    if ($y(".popover:hover").length) {
         return false;
     }
 });
 
-$('body').on('mouseleave', '.popover', function () {
-    $('.popover').popover('hide');
+$y('body').on('mouseleave', '.popover', function () {
+    $y('.popover').popover('hide');
 });
