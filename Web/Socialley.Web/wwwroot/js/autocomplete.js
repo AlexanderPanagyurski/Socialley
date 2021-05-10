4831fd641669ece22c87a88ec24547b65da67c5b
AlexanderPanagyurski <alexander.panagyurski@gmail.com>
2021-05-10 20:23:14 +0300

    Move autocmoplete js script to file

---
+++ Web/Socialley.Web/wwwroot/js/autocomplete.js
@@ -0,0 +1,15 @@
+﻿$(function () {
+    var availableTags = {};
+    $.ajax({
+        url: "/api/searches",
+        type: "GET",
+        data: JSON.stringify(availableTags),
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function (data) {
+            $("#tags").autocomplete({
+                source: data
+            });
+        }
+    });
+});
