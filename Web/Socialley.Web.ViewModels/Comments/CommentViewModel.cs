35103888ff1985536240f97ae4f2c9a56b659094
AlexanderPanagyurski <alexander.panagyurski@gmail.com>
2021-07-06 19:00:38 +0300

    Implement Add Comments

---
+++ Web/Socialley.Web.ViewModels/Comments/CommentViewModel.cs
@@ -0,0 +1,28 @@
+﻿namespace Socialley.Web.ViewModels.Comments
+{
+    using Ganss.XSS;
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class CommentViewModel
+    {
+        public string CommentId { get; set; }
+
+        public string PostId { get; set; }
+
+        public string UserId { get; set; }
+
+        public string UserName { get; set; }
+
+        public string UserProfileUrl { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public DateTime? ModifiedOn { get; set; }
+
+        public string Content { get; set; }
+
+        public string SanitizedContent => new HtmlSanitizer().Sanitize(this.Content);
+    }
+}
---
+++ Web/Socialley.Web.ViewModels/Comments/CreateCommentInputModel.cs
@@ -0,0 +1,18 @@
+﻿namespace Socialley.Web.ViewModels.Comments
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    using Socialley.Data.Models;
+
+    public class CreateCommentInputModel
+    {
+        public string PostId { get; set; }
+
+        public string Content { get; set; }
+
+        public string ParentId { get; set; }
+
+    }
+}
